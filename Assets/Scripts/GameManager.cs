668bb42c7137a55212c3cfa2bc4e6d7521804396
Ratsarun Thipdumrong <mewgamer17@gmail.com>
2026-04-03 17:00:35 +0700

    V1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     [Header("UI Elements")]
     // NOTE: TextMeshProUGUI requires "using TMPro"
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreValue;
     // NOTE: TextMeshProUGUI requires "using TMPro"
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
@@ -22,6 +23,8 @@ public class GameManager : MonoBehaviour
     public Button easyButton;
     public Button mediumButton;
     public Button hardButton;
+    private Coroutine currentCoroutine;
+    public Target target;
 
     private int score;
     private bool isGameActive = true;
@@ -35,16 +38,34 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         StartGame();
+        score = 0;
+        scoreText.text = "Score : " + score;
     }
 
     void StartGame()
     {
         StartCoroutine(SpawnTargets());
     }
-
     IEnumerator SpawnTargets()
     {
-        yield return null;
+        while (isGameActive)
+        {
+            int index = Random.Range(0, targets.Count);
+            var prefed = targets[index];
+            Instantiate(prefed);
+            // yield return new WaitUntil(() => Input.GetKey(KeyCode.Space));
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+    public void UpdateScore(int s)
+    {
+        score += s;
+        scoreValue.text = s.ToString(); 
+        scoreText.text = "Score : " + score;
+    }
+    public bool CheckDestroy()
+    {
+        return score != 0;
     }
 }
 
